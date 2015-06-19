4e51dd3447d16de342dec5703860683fc8bbfb9d
Robin Stevenson <rob.n.stevenson@gmail.com>
2015-06-19 16:42:37 +0100

    adding codebook and updating run_analysis and README

---
+++ CodeBook.md
@@ -0,0 +1,47 @@
+## Variable included in this dataset
+
+The variables included in the tablemeans.txt dataset are calculated from the UCI HAR Dataset.
+Specifically, these data are created from the means and standard deviations(both in each direction)
+provided in that dataset.
+For information about how that dataset was created see "UCI HAR Dataset/features_info.txt"
+
+'-XYZ' is used to denote 3-axial signals in the x, y and z directions.
+
+#### Labels:
+* subject: identifies the subject who performed the activity. Its range is 1 to 30
+* activity: identifies the activity performed. Options are "walking", "walking up", "walking down", "sitting", "standing" and "laying"
+
+#### Features:
+This data set contains the mean of each of these features over each subject and activity. 
+Note that before the mean is calculated all of these data have been normalised to lie between -1 and 1
+and so the means of these data will have an even smaller range. The units of these measurements are 
+therefore unclear.
+
+The features in this dataset are 
+* tBodyAcc-XYZ
+* tGravityAcc-XYZ
+* tBodyAccJerk-XYZ
+* tBodyGyro-XYZ
+* tBodyGyroJerk-XYZ
+* tBodyAccMag
+* tGravityAccMag
+* tBodyAccJerkMag
+* tBodyGyroMag
+* tBodyGyroJerkMag
+* fBodyAcc-XYZ
+* fBodyAccJerk-XYZ
+* fBodyGyro-XYZ
+* fBodyAccMag
+* fBodyAccJerkMag
+* fBodyGyroMag
+* fBodyGyroJerkMag
+
+The following have been calculated for each feature
+* mean(): the mean value of the feature
+* std(): the standard deviation of the feature
+
+Each of these calculated data is then grouped by activity and subject, 
+and then the mean for each group is calculated (yes, we are calculating
+the mean of a mean, and the mean of a standard deviation).
+
+
--- run_analysis.R
+++ run_analysis.R
@@ -1,28 +1,38 @@
 library(plyr)
 
+# set directory to be the current directory of the script
+script.dir <- dirname(sys.frame(1)$ofile)
+setwd(script.dir)
+
+
 # read in data
-# filepathTestData <- "./data/UCI HAR Dataset/test/X_test.txt"
-# filepathTestSubjects <- "./data/UCI HAR Dataset/test/subject_test.txt"
-# filepathTestLabels <- "./data/UCI HAR Dataset/test/y_test.txt"
-# filepathTrainData <- "./data/UCI HAR Dataset/Train/X_Train.txt"
-# filepathTrainSubjects <- "./data/UCI HAR Dataset/Train/subject_Train.txt"
-# filepathTrainLabels <- "./data/UCI HAR Dataset/Train/y_Train.txt"
-# mydataTest <- read.table(filepathTestData)
-# mydataTestSubjects<-read.table(filepathTestSubjects)
-# mydataTestLabels<-read.table(filepathTestLabels)
-# mydataTrain <- read.table(filepathTrainData)
-# mydataTrainSubjects<-read.table(filepathTrainSubjects)
-# mydataTrainLabels<-read.table(filepathTrainLabels)
+filepathTestData <- "./data/UCI HAR Dataset/test/X_test.txt"
+filepathTestSubjects <- "./data/UCI HAR Dataset/test/subject_test.txt"
+filepathTestLabels <- "./data/UCI HAR Dataset/test/y_test.txt"
+filepathTrainData <- "./data/UCI HAR Dataset/Train/X_Train.txt"
+filepathTrainSubjects <- "./data/UCI HAR Dataset/Train/subject_Train.txt"
+filepathTrainLabels <- "./data/UCI HAR Dataset/Train/y_Train.txt"
+filepathFeatures <- "./data/UCI HAR Dataset/features.txt"
+dataTest <- read.table(filepathTestData)
+TestSubjects<-read.table(filepathTestSubjects)
+TestLabels<-read.table(filepathTestLabels)
+dataTrain <- read.table(filepathTrainData)
+TrainSubjects<-read.table(filepathTrainSubjects)
+TrainLabels<-read.table(filepathTrainLabels)
+Features<-read.table(filepathFeatures,colClasses="character")
 
 # merge data sets
-mydata<- rbind(mydataTest,mydataTrain)
-mydataSubjects<- rbind(mydataTestSubjects,mydataTrainSubjects)
-mydataLabels<- rbind(mydataTestLabels,mydataTrainLabels)
+mydata<- rbind(dataTest,dataTrain)
+Subjects<- rbind(TestSubjects,TrainSubjects)
+Labels<- rbind(TestLabels,TrainLabels)
+
+
+# make logical vector of whether feature names contain "mean" or "std"
+meanorstd<-grepl("mean|std",Features[,2])
 
 # extract only the rows, and add subject and activity labels
-subdata<-cbind(mydataSubjects,mydataLabels,mydata[,1:6])
-names(subdata)<-c("subject","activity","meanx","meany","meanz",
-                  "standarddeviationx","standarddeviationy","standarddeviationz")
+subdata<-cbind(Subjects,Labels,mydata[,meanorstd])
+names(subdata)<-c("subject","activity",Features[meanorstd,2])
 # convert to subject and activity to factors
 subdata$subject<-factor(subdata$subject)
 subdata$activity<-factor(subdata$activity)
@@ -30,7 +40,7 @@ levels(subdata$activity)<-c("walking","walking up","walking down", "sitting",
                             "standing", "laying")
 
 # make new data set with average of each variable
-means<-aggregate(subdata[,3:8],by=list(subdata$subject,subdata$activity),mean)
+means<-aggregate(subdata[,-(1:2)],by=list(subdata$subject,subdata$activity),mean)
     # aggregate is similar to tapply, but can work on multiple columns at once
 # rename first to columns back to subject and activity
 names(means)[1]<-"subject"
